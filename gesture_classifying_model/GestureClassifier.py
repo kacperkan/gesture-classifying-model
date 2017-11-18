aa062ff06fa173fcc63a728ddcfd2a4637389e0f
Kacper <kacper1095@gmail.com>
2017-11-18 19:50:06 +0100

    Added possible time inteval instead of frames to ommit

--- gesture_classifying_model/GestureClassifier.py
+++ gesture_classifying_model/GestureClassifier.py
@@ -3,6 +3,7 @@ from . import custom_objectives
 
 import numpy as np
 import cv2
+import time
 
 
 class GestureClassifier(object):
@@ -11,32 +12,27 @@ class GestureClassifier(object):
     __MODEL_URL_KEY = 'MODEL_URL'
     __MODEL_INFO = 'MODEL_INFO'
     __MAX_CONTEXT_SIZE_KEY = 'MAX_CONTENT_SIZE'
-    __FRAMES_TO_OMMIT = 'FRAMES_TO_OMMIT'
     __IMAGE_INPUT_SHAPE_KEY = 'image_input_shape'
 
-    def __init__(self):
+    def __init__(self, classification_time_interval=3):
         self.output = None
         self.config = downloader.download_config(GestureClassifier.__CONFIG_FILE_URL)
         self.out_key = GestureClassifier.__OUT_KEY
         self.context = []
-        self.max_context_size = self.config[GestureClassifier.__MAX_CONTEXT_SIZE_KEY]
-        self.frames_to_ommit = self.config[GestureClassifier.__FRAMES_TO_OMMIT]
         self.frames_ommitted = 0
         self.input_shape = self.config[GestureClassifier.__MODEL_INFO][GestureClassifier.__IMAGE_INPUT_SHAPE_KEY]
         self.image_width = self.input_shape[2]
         self.image_height = self.input_shape[1]
 
         self.model = self.load_model()
+        self.previous_classifying_timestamp = time.time()
+        self.classification_time_interval = classification_time_interval
 
     def transform(self, X, **transform_params):
-        if len(self.context) < self.max_context_size:
-            if self.frames_ommitted == self.frames_to_ommit:
-                self.context.append(X[-1])
-                self.frames_ommitted = 0
-            else:
-                self.frames_to_ommit = 0
+        time_dif = time.time() - self.previous_classifying_timestamp
+        if time_dif < self.classification_time_interval:
             return None
-
+        self.previous_classifying_timestamp = time.time()
         inputs = X[-1]
         inputs = cv2.resize(inputs, (self.image_width, self.image_height))
         inputs = inputs.transpose((2, 0, 1)) / 255.
